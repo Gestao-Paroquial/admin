357eb55b76d3d593e0f08583d6cbe210c84b29a5
Leonardo Cardoso <leocardosoti@gmail.com>
2017-11-16 21:20:00 -0200

    eliminando componentes não utilizados

--- src/components/UIComponents/SidebarPlugin/index.js
+++ src/components/UIComponents/SidebarPlugin/index.js
@@ -13,21 +13,21 @@ const SidebarStore = {
 			icon: 'fa fa-users',
 			path: '/admin/visitantes'
 		},
-		{
-			name: 'Participantes',
-			icon: 'fa fa-users',
-			path: '/admin/participantes'
-		},
+			// {
+			// 	name: 'Participantes',
+			// 	icon: 'fa fa-users',
+			// 	path: '/admin/participantes'
+			// },
 		{
 			name: 'Membros Pastorais',
 			icon: 'fa fa-users',
 			path: '/admin/membros-pastorais'
 		},
-		{
-			name: 'Dizimistas',
-			icon: 'fa fa-user-plus',
-			path: '/admin/dizimistas'
-		},
+		// {
+		// 	name: 'Dizimistas',
+		// 	icon: 'fa fa-user-plus',
+		// 	path: '/admin/dizimistas'
+		// },
 		{
 			name: 'Pastorais',
 			icon: 'fa fa-building-o',
@@ -38,36 +38,36 @@ const SidebarStore = {
 			icon: 'fa fa-globe',
 			path: '/admin/comunidades'
 		},
-		{
-			name: 'User Profile',
-			icon: 'ti-user',
-			path: '/admin/stats'
-		},
-		{
-			name: 'Table List',
-			icon: 'ti-view-list-alt',
-			path: '/admin/table-list'
-		},
-		{
-			name: 'Typography',
-			icon: 'ti-text',
-			path: '/admin/typography'
-		},
-		{
-			name: 'Icons',
-			icon: 'ti-pencil-alt2',
-			path: '/admin/icons'
-		},
-		{
-			name: 'Maps',
-			icon: 'ti-map',
-			path: '/admin/maps'
-		},
-		{
-			name: 'Notifications',
-			icon: 'ti-bell',
-			path: '/admin/notifications'
-		}
+		// {
+		// 	name: 'User Profile',
+		// 	icon: 'ti-user',
+		// 	path: '/admin/stats'
+		// },
+		// {
+		// 	name: 'Table List',
+		// 	icon: 'ti-view-list-alt',
+		// 	path: '/admin/table-list'
+		// },
+		// {
+		// 	name: 'Typography',
+		// 	icon: 'ti-text',
+		// 	path: '/admin/typography'
+		// },
+		// {
+		// 	name: 'Icons',
+		// 	icon: 'ti-pencil-alt2',
+		// 	path: '/admin/icons'
+		// },
+		// {
+		// 	name: 'Maps',
+		// 	icon: 'ti-map',
+		// 	path: '/admin/maps'
+		// },
+		// {
+		// 	name: 'Notifications',
+		// 	icon: 'ti-bell',
+		// 	path: '/admin/notifications'
+		// }
 	],
 	displaySidebar(value) {
 		this.showSidebar = value;
