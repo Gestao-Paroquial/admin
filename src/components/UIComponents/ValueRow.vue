8b25799944d9157b9b56fb7eea4ec36e63d96978
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-03-15 13:12:59 -0300

    alguns ajustes e adicionando aba de extrato

--- src/components/UIComponents/ValueRow.vue
+++ src/components/UIComponents/ValueRow.vue
@@ -1,63 +1,31 @@
 <template>
   <div class="row">
-    <value-box
-    grid="col-sm-12 col-md-4"
-    color-class="bg-green"
-    icon-class="fa fa-bank"
-    :value="formatToPrice(credit)"
-    text="Total de Créditos">
+    <value-box grid="col-sm-12 col-md-4" color-class="bg-green" icon-class="fa fa-bank" :value="formatToPrice(credit)" text="Total de Créditos">
     </value-box>
 
-    <value-box
-    grid="col-sm-12 col-md-4"
-    color-class="bg-red"
-    icon-class="fa fa-credit-card"
-    :value="formatToPrice(debt)"
-    text="Total de Débitos">
+    <value-box grid="col-sm-12 col-md-4" color-class="bg-red" icon-class="fa fa-credit-card" :value="formatToPrice(debt)" text="Total de Débitos">
     </value-box>
 
-    <value-box
-    grid="col-sm-12 col-md-4"
-    color-class="bg-blue"
-    icon-class="fa fa-money"
-    :value="formatToPrice(total)"
-    text="Valor Consolidado">
+    <value-box grid="col-sm-12 col-md-4" color-class="bg-blue" icon-class="fa fa-money" :value="formatToPrice(total)" text="Valor Consolidado">
     </value-box>
   </div>
 </template>
 <script>
-import axios from 'axios';
 import ValueBox from './ValueBox';
-import { billingSummaryApiUrl } from '../../api-url';
 
 export default {
   components: {
     ValueBox,
   },
-
-  data() {
-    return {
-      credit: 0,
-      debt: 0,
-      total: 0,
-    };
+  props: {
+    credit: Number,
+    debt: Number,
+    total: Number,
   },
   methods: {
     formatToPrice(value) {
       return `R$ ${value.toFixed(2)}`;
     },
-  },
-  created() {
-    axios
-      .get(billingSummaryApiUrl)
-      .then(({ data }) => {
-        this.credit = data.credit;
-        this.debt = data.debt;
-        this.total = data.credit - data.debt;
-      })
-      .catch((response) => {
-        console.log(response);
-      });
   },
 };
 </script>
