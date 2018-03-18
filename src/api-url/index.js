c8b57fcdd3b6f660f46ee06290ed916ffbc9ccfb
Leonardo Cardoso <leocardosoti@gmail.com>
2018-03-18 16:06:01 -0300

    adicionando nova rota

--- src/api-url/index.js
+++ src/api-url/index.js
@@ -1,14 +1,21 @@
-
+export const billingCyclesUrl = 'http://localhost:3003';
 export const backEndUrl = 'http://localhost:8000';
+
+// URL
 export const visitantesApiUrl = `${backEndUrl}/api/visitantes`;
 export const comunidadesApiUrl = `${backEndUrl}/api/comunidades`;
 export const pastoraisApiUrl = `${backEndUrl}/api/pastorais`;
 export const membrosPastoraisApiUrl = `${backEndUrl}/api/membrosPastorais`;
 export const mensagensParocoApiUrl = `${backEndUrl}/api/mensagensParoco`;
-export const billingCyclesApiUrl = 'http://localhost:3003/api/billingCycles';
-export const billingSummaryApiUrl = 'http://localhost:3003/api/billingSummary';
+export const billingCyclesApiUrl = `${billingCyclesUrl}/api/billingCycles`;
+export const billingSummaryApiUrl = `${billingCyclesUrl}/api/billingSummary`;
+export const findBillingCycleByComunity = `${billingCyclesUrl}/api/billingCycles/findByComunidade`;
 export const eventosHomeApiUrl = `${backEndUrl}/api/eventosHome`;
 export const uploadImageApiUrl = `${backEndUrl}/api/uploadImagem`;
 export const loginApiUrl = `${backEndUrl}/api/login`;
 export const logoutApiUrl = `${backEndUrl}/api/logout`;
 export const validateTokenUrl = `${backEndUrl}/api/validateToken`;
+
+// Helpers
+export const findBillingCycleByComunityId = comunidadeId => `${findBillingCycleByComunity}?id=${comunidadeId}`
+;
