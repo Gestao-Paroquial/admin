b46d0164bdcff92d9ed087f60218eec86775067e
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-03-20 15:35:06 -0300

    adding production url

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BILLING_CYCLE_API_URI: '"http://localhost:3003"'
+  BILLING_CYCLE_API_URI: '"http://localhost:3003"',
+  BACK_END_API_URI: '"http://localhost:8000"'
 })
