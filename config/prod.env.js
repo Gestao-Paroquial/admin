3c29c8f0726ddcc55a0c75388ae2d6b46cbe8bdc
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-03-22 10:43:01 -0300

    utilizando back end  da digital ocean

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BILLING_CYCLE_API_URI: '"http://ec2-34-217-99-46.us-west-2.compute.amazonaws.com"',
-  BACK_END_API_URI: '"http://back-end.us-west-2.elasticbeanstalk.com"',
+  BILLING_CYCLE_API_URI: '"http://165.227.197.233:3003"',
+  BACK_END_API_URI: '"http://165.227.197.233"',
 }
