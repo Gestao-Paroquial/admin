1fadec2721ab024d7f16d5cd9c00865c764d7439
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-04-05 11:49:14 -0300

    adicionando informações dos aniversariantes

--- src/components/UIComponents/AniversariantesDoMes.vue
+++ src/components/UIComponents/AniversariantesDoMes.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-md-4 col-sm-12 aniversariantes">
-    <h4>Aniversariantes do mês de {{capitalize(getTheActualMonth())}}</h4>
+    <h4>Aniversariantes do Mês de {{capitalize(getTheActualMonth())}}</h4>
     <table class="table">
       <thead>
         <th><i class="fa fa-calendar"/></th>
@@ -8,22 +8,44 @@
         <th><i class="fa fa-birthday-cake"/></th>
       </thead>
       <tbody>
-        <tr>
-          <td>01</td>
-          <td>José</td>
-          <td>50 anos</td>
+        <tr v-for="aniversariante in aniversariantes" :key="aniversariante.id">
+          <td>{{getBirthday(aniversariante.data_Nascimento)}}</td>
+          <td>{{aniversariante.nome}}</td>
+          <td>{{calcAge(aniversariante.data_Nascimento)}} anos</td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
 <script>
+import axios from '@/plugins/axios';
+import { aniversariantesUrl } from './../../api-url';
+
 export default {
+  data() {
+    return {
+      aniversariantes: [],
+    };
+  },
+  mounted() {
+    axios.get(`${aniversariantesUrl}/${new Date().getMonth() + 1}`)
+      .then(({ data }) => {
+        this.aniversariantes = data;
+      });
+  },
   methods: {
     getTheActualMonth() {
       const date = new Date();
       const month = date.toLocaleDateString('pt-BR', { month: 'long' });
       return month;
+    },
+    getBirthday(date) {
+      return date.split('-')[2];
+    },
+    calcAge(dateString) {
+      /* eslint no-bitwise: ["error", { "allow": ["~"] }] */
+      const birthday = +new Date(dateString);
+      return ~~((Date.now() - birthday) / (31557600000));
     },
   },
 };
