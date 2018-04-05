447933789cb781bf1d836ac98316e71e473e2bd0
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-04-05 10:15:08 -0300

    componentizando os botões de formulário

--- src/globalComponents.js
+++ src/globalComponents.js
@@ -2,6 +2,7 @@ import fgInput from './components/UIComponents/Inputs/formGroupInput';
 import DropDown from './components/UIComponents/Dropdown';
 import Loader from './components/UIComponents/Loader';
 import BackButton from './components/UIComponents/BackButton';
+import FormButtons from './components/UIComponents/FormButtons';
 import GoToAddPage from './components/UIComponents/Buttons/GoToAddPage';
 /**
  * You can register global components here and use them as a plugin in your main Vue instance
@@ -14,6 +15,7 @@ const GlobalComponents = {
     Vue.component('loader', Loader);
     Vue.component('back-button', BackButton);
     Vue.component('go-to-add-page', GoToAddPage);
+    Vue.component('form-buttons', FormButtons);
   },
 };
 
