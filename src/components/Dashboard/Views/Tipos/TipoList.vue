7dcd3dd40d2e1a6eddefb5d45330e27b01f65857
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-04-13 11:10:37 -0300

    alterando tipos

--- src/components/Dashboard/Views/Tipos/TipoList.vue
+++ src/components/Dashboard/Views/Tipos/TipoList.vue
@@ -10,13 +10,15 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="tipoItem in tipo" :key="tipoItem.id">
-          <td>{{tipoItem.descricao}}</td>
+        <tr v-for="(tipoItem) in tipo" :key="tipoItem.id">
           <td>
-            <button  class="btn btn-simple btn-xs btn-warning btn-icon view" >
-              <i class="ti-pencil-alt" />
+            <input class="form-control border-input" :disabled="!tipoItem.edit" @change="handleChange(tipoItem)" v-model="tipoItem.descricao" />
+          </td>
+          <td>
+            <button class="btn btn-simple btn-xs btn-warning btn-icon view">
+              <i class="ti-pencil-alt" @click="habilitarEdicao(tipoItem)" />
             </button>
-            <button  class="btn btn-simple btn-xs btn-danger btn-icon remove" >
+            <button class="btn btn-simple btn-xs btn-danger btn-icon remove">
               <i class="ti-close" />
             </button>
           </td>
@@ -26,10 +28,30 @@
   </div>
 </template>
 <script>
+import axios from '@/plugins/axios';
+
 export default {
   props: {
     tipo: Array,
+    endPoint: String,
     title: String,
+  },
+  methods: {
+    habilitarEdicao(tipoItem) {
+      tipoItem.edit = true;
+      this.$forceUpdate();
+    },
+    handleChange(tipoItem) {
+      axios
+        .put(`${this.endPoint}/${tipoItem.id}`, JSON.stringify(tipoItem), {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        .then(({ data }) => {
+          this.$notifications.notify(this.notificationConfig(data.message));
+        });
+    },
   },
 };
 </script>
--- src/components/Dashboard/Views/Tipos/index.vue
+++ src/components/Dashboard/Views/Tipos/index.vue
@@ -5,41 +5,31 @@
 
     <div class=" card card-plain">
       <div class="row">
-        <tipo-list :tipo="tipos.dependentes" title="Dependentes"/>
-        <tipo-list :tipo="tipos.membros" title="Membros"/>
-        <tipo-list :tipo="tipos.eventos" title="Eventos"/>
+        <tipo-list :tipo="tipos.dependentes" title="Dependentes" :endPoint="endPoints.tiposDependenteUrl"/>
+        <tipo-list :tipo="tipos.membros" title="Membros" :endPoint="endPoints.tiposMembroUrl"/>
+        <tipo-list :tipo="tipos.eventos" title="Eventos" :endPoint="endPoints.tiposEventoUrl"/>
       </div>
     </div>
   </div>
 </template>
 <script>
-import PaperTable from '@/components/UIComponents/PaperTable';
-import Modal from '@/components/UIComponents/Modal/Modal';
 import axios from 'axios';
-import debounce from 'lodash.debounce';
 import TipoList from './TipoList';
-import { tiposUrl } from './../../../../api-url';
-
-const comunidadesHeaders = ['id', 'nome', 'email', 'cnpj', 'cep'];
+import { tiposUrl, tiposDependenteUrl, tiposMembroUrl, tiposEventoUrl } from './../../../../api-url';
 
 export default {
   components: {
-    PaperTable,
-    Modal,
     TipoList,
   },
   data() {
     return {
-      showModalAdd: false,
-      showModalUpdate: false,
+      endPoints: {
+        tiposDependenteUrl, tiposMembroUrl, tiposEventoUrl,
+      },
       tipos: {},
-      table: { data: [] },
       showLoader: true,
-      filterProperty: 'id',
-      termToSearch: '',
-      comunidadesHeaders,
+
       title: 'Lista com todos os tipos do sistema',
-      subTitle: 'Aqui você ira encontrar a lista de comunidades completa',
     };
   },
   mounted() {
@@ -52,30 +42,6 @@ export default {
       .catch((error) => {
         console.log(error);
       });
-  },
-  watch: {
-    termToSearch: debounce(
-      function filter() {
-        const comunidadesFiltrados = this.comunidades.filter(
-          obj =>
-            (obj[this.filterProperty]
-              ? obj[this.filterProperty]
-                .toString()
-                .toLowerCase()
-                .match(this.termToSearch.toLowerCase())
-              : undefined),
-        );
-        this.updateTable(comunidadesFiltrados);
-      },
-      // Este é o número de milissegundos que aguardamos para
-      // que o usuário pare de digitar
-      300,
-    ),
-  },
-  methods: {
-    updateTable(comunidades) {
-      this.table.data = [...comunidades];
-    },
   },
 };
 </script>
