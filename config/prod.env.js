16602b66345c1e7c6d775b4efc4dfa6b704e33ed
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-04-30 15:15:40 -0300

    change prod url

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   BILLING_CYCLE_API_URI: '"http://165.227.197.233:3003"',
-  BACK_END_API_URI: '"http://paroquiasle.org.br"',
+  BACK_END_API_URI: '"http://laravel.paroquiasle.org.br"',
 }
