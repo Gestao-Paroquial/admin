be82e47e1f6c80c233b0428ac2f0596f394a3aed
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-24 21:11:18 -0300

    criando lógica para salvar e alterar preços

--- src/components/Dashboard/Views/Pedidos/index.vue
+++ src/components/Dashboard/Views/Pedidos/index.vue
@@ -1,8 +1,44 @@
 <template>
   <div class="col-md-12">
     <loader v-if="showLoader" />
+    <div class="row">
+      <div class="col-md-6 col-sm-12">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th colspan="2">Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Casamento</td>
+              <td>
+                <input v-model="casamento.preco" :disabled="!casamento.editable" @change="handleChange(casamento)" @blur="handleBlur(casamento)" placeholder="Informe o Valor " type="number" :min="0" :step="'0.01'" />
+              </td>
+              <td>
+              <button class="btn btn-simple btn-xs btn-warning btn-icon view">
+                <i class="ti-pencil-alt" @click="habilitarEdicao(casamento)" />
+              </button>
+            </td>
+            </tr>
+            <tr>
+              <td>Batismo</td>
+              <td>
+                <input v-model="batismo.preco" @change="handleChange(batismo)" @blur="handleBlur(batismo)" :disabled="!batismo.editable"  placeholder="Informe o Valor " type="number" :min="0" :step="'0.01'" />
+              </td>
+              <td>
+                <button class="btn btn-simple btn-xs btn-warning btn-icon view">
+                  <i class="ti-pencil-alt" @click="habilitarEdicao(batismo)" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
     <h4 class="title">{{ title }}</h4>
-    <pedidos-table :pedidos="pedidos"/>
+    <pedidos-table :pedidos="pedidos" />
     <div class="row">
       <solicitacoes title="Casamentos" :solicitacoes="casamentos"></solicitacoes>
       <solicitacoes title="Batismos" :solicitacoes="batismos"></solicitacoes>
@@ -23,13 +59,26 @@ export default {
       batismos: [],
       pedidos: [],
       showLoader: true,
-      title: 'Lista de pedidos aguardando pagamento',
+      title: 'Lista de pedidos',
+      precoCasamento: 50,
+      precoBatismo: 50,
+      casamento: {
+        preco: 50,
+        editable: false,
+      },
+      batismo: {
+        preco: 50,
+        editable: false,
+      },
     };
   },
   mounted() {
     this.getPedidos();
   },
   methods: {
+    habilitarEdicao(item) {
+      item.editable = true;
+    },
     getPedidos: async function getPedidos() {
       const { data } = await axios.get(pedidosUrl);
       this.pedidos = data.filter(pedido => pedido.code);
@@ -38,9 +87,14 @@ export default {
 
       this.showLoader = false;
     },
+    handleChange(item) {
+      console.log(item);
+    },
+    handleBlur(item){
+      item.editable = false;
+    }
   },
-};
+}; 
 </script>
 <style>
-
 </style>
